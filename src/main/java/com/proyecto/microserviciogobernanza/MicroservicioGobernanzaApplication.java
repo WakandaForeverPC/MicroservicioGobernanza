1649705becdd575e07acfbdbac3eed9bcc1bf957
Samuu10 <svallmun@myuax.com>
2024-12-10 18:25:28 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microserviciogobernanza/MicroservicioGobernanzaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciogobernanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioGobernanzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioGobernanzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microserviciogobernanza/MicroservicioGobernanzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciogobernanza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioGobernanzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
